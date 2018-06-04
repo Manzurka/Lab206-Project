f39c7c6ffe7d1e56a54499016837f888f400278b
Zakirova <manzuraz@amazon.com>
2018-06-04 09:38:30 -0700

    first commit

---
+++ src/main/java/com/lab206/controllers/UserController.java
@@ -0,0 +1,8 @@
+package com.lab206.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
---
+++ src/main/java/com/lab206/models/User.java
@@ -0,0 +1,10 @@
+package com.lab206.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+}
