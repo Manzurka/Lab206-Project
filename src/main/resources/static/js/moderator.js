70df919a2fe976d812e1faa8b9c1c448ee606c6c
Darnell <darnellwillis96@gmail.com>
2018-06-29 09:01:23 -0700

    ajax working for feedback table modal

---
+++ src/main/resources/static/js/moderator.js
@@ -0,0 +1,14 @@
+$('.c_feedback').click(function() {
+	var displayFeedback = $(this).attr('data-feed-id');
+	console.log(displayFeedback)
+	$.ajax({
+		url: "/post/feedbacks/" + displayFeedback
+	}).then(function(feed) {
+		console.log(feed);
+		console.log("Is this reviewed: " + feed.reviewed);
+		$('#feedContent').html(feed.content);
+		$('#feedRating').html(feed.rating);
+		$('#feedSubmitter').html(feed.feedbackCreator.firstName + ' ' + feed.feedbackCreator.lastName);
+		$('#feedReviewed').html(feed.reviewed);
+	});
+});
